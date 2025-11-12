f32a3346193daa81697e5f5c61b6ddcfec04d82b
Timothy Hastie <timothyhastie@Timothys-MacBook-Pro.local>
2025-11-11 17:06:10 -0800

    CHROME WORKING STARTING SAFARI FIX

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -271,10 +271,7 @@ export default function AuthCallback() {
           const safari = isSafariBrowser();
           if (safari) {
             console.log("[AuthCallback] Safari session captured; forcing hard reload to /");
-            window.location.replace("/");
-            setTimeout(() => {
-              window.location.href = "/";
-            }, 50);
+            window.location.href = "/?source=auth";
           } else {
             window.history.replaceState({}, document.title, "/");
             nav("/", { replace: true });
